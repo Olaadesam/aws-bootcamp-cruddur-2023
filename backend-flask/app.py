d01d43de29bcb8307d2d555b62e59b82be301295
Olaadesam <Olaadesam@gmail.com>
2023-04-14 22:02:02 +0000

    Implementing Rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -44,6 +44,12 @@ import requests
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+#Rollbar implementation
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
+
 # instrument: Set up the Honeycomb exporter:_homework challenge
 
 honeycomb_key = os.environ.get("HONEYCOMB_API_KEY")
@@ -64,6 +70,7 @@ provider.add_span_processor(processor)
 # X_RAY ---------
 #xray_url = os.getenv("AWS_XRAY_URL")
 #xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
 
 
 # Show log within the backend flask STDOUT honeycomb
@@ -158,6 +165,32 @@ cors = CORS(
   #  LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
    # return response
 
+# Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+
+# RollBar test -------
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
+
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
@@ -196,7 +229,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run(Logger=LOGGER)
+  data = HomeActivities.run()
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
